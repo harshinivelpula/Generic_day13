7cf1f400a6206a8a9c7e19c81e92287fb0d5731b
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-12-02 11:53:03 +0530

    find max using 3 integers

--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("welcome to generic problem");
+
+            int output = maxinteger.MaxIntegerNumber(28,8,11);
+            Console.WriteLine("\nMax integer number is: " + output);
         }
     }
 }
