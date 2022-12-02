bab065e7c1665c64d4eacf97ae6e75d3bc8676ad
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-12-02 11:46:43 +0530

    print array of int double char using generics

--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
-﻿namespace Generic
+﻿using System.Collections;
+
+namespace Generic
 {
     class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("welcome to generic problem");
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+            char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+            arraygenerics.toPrint(intArray);
+            arraygenerics.toPrint(doubleArray);
+            arraygenerics.toPrint(charArray);
         }
     }
 }
