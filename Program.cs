aaf3849b29e93a34891aa654c61b166264c89d0f
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-12-02 15:28:51 +0530

    creating genericclass to take 3 variables

--- Program.cs
+++ Program.cs
@@ -4,7 +4,14 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("welcome to generic problem");
+            int intArray = Refactors<int>.MaximumValue(8,11,28);
+            Console.WriteLine("\nMax integer number is: " + intArray);
+
+            double output = Refactors<double>.MaximumValue(12.6,13.8,18.20);
+            Console.WriteLine("\nMax floating number is: " + output);
+
+            char strArr = Refactors<char>.MaximumValue('s','h','k');
+            Console.WriteLine("\nMax string is: " + strArr);
         }
     }
 }
