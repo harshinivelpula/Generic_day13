23d904f57816e40eff66b7e4b5490b6b6e42a73e
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-12-02 15:01:50 +0530

    find max using 3 floats

--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("welcome to generic problem");
+
+            double doubleoutput = maxfloat.MaximumFloatNumber(111, 2, 22);
+            Console.WriteLine(doubleoutput);
         }
     }
 }
