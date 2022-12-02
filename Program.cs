684ce3b00789b701d9fc1076e4821be252521694
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-12-02 11:26:48 +0530

    welcome message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Generic
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to generic problem");
+        }
+    }
+}
