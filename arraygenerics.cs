bc56aaa8f136da80fc9e6bf200a825c90d79fcb0
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-12-02 11:47:20 +0530

    print array of int double char using generics

---
+++ arraygenerics.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    public class arraygenerics
+    {
+        public static void toPrint<T>(T[] inputArray)
+        {
+            foreach (var element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("----------------------------------");
+        }
+    }
+}
