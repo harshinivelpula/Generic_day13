9a8040d00f90971b5592c01da2e7d658e9b08575
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-12-02 15:07:21 +0530

    find max using 3 strings

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("welcome to generic problem");
+            string stringoutput = maxstring.MaximumStringNumber("28", "33", "40");
+            Console.WriteLine(stringoutput);
         }
     }
 }
